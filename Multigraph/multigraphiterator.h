26061a14f52bd1a6f4fb823d15ff97e9905116dc
LepilkinaElena <eklepilkina@gmail.com>
2016-04-29 23:20:08 +0300

    Дописала метод и комментарии.

--- Multigraph/multigraphiterator.h
+++ Multigraph/multigraphiterator.h
@@ -1,3 +1,6 @@
+/*!\file multigraphiterator.h
+*\brief Файл с классом для описания итератора мультиграфа
+*/
 #ifndef MULTIGRAPHITERATOR_H
 #define MULTIGRAPHITERATOR_H
 
@@ -10,22 +13,57 @@ namespace Multigraph {
     template <class T>
     class Multigraph;
 
+    /*!\class MultigraphIterator
+    *\brief Класс итератора мультиграфа
+    *\ref MultigraphIterator
+    */
     template<typename ValueType>
     class MultigraphIterator: public std::iterator<std::input_iterator_tag, std::pair<const ValueType, Edge<ValueType> >>
     {
         friend class Multigraph<ValueType>;
     private:
         typedef typename std::multimap<ValueType, Edge<ValueType> >::iterator MapIterator;
+
+        /*!\var MultigraphIterator::mapIterator
+        *\brief итератор на элемент в карте
+        */
         MapIterator mapIterator;
 
         MultigraphIterator(MapIterator mapIterator);
     public:
         MultigraphIterator(const MultigraphIterator &it);
 
+        /*!\fn  operator!=(MultigraphIterator const& other) const;
+        *\brief Проверка итераторов на неравенство
+        *\param [in] other - другой итератор
+        *\return признак, не равен ли текущий итератор другому
+        */
         bool operator!=(MultigraphIterator const& other) const;
+
+        /*!\fn  operator==(MultigraphIterator const& other) const;
+        *\brief Проверка итераторов на равенство
+        *\param [in] other - другой итератор
+        *\return признак, равен ли текущий итератор другому
+        */
         bool operator==(MultigraphIterator const& other) const;
+
+        /*!\fn  operator*() const;
+        *\brief Получение значения по итератору
+        *\return значение элемента, на который указывает итератор
+        */
         typename MultigraphIterator::reference operator*() const;
+
+        /*!\fn  operator++();
+        *\brief Получение итератора на следующий элемент
+        *\return итератор на следующий элемент
+        */
         MultigraphIterator& operator++();
+
+        /*!\fn  operator--();
+        *\brief Получение итератора на предыдущий элемент
+        *\return итератор на предыдущий элемент
+        */
+        MultigraphIterator& operator--();
     };
 
     template<typename ValueType>
@@ -58,6 +96,13 @@ namespace Multigraph {
     MultigraphIterator<ValueType> &MultigraphIterator<ValueType>::operator++()
     {
         ++mapIterator;
+        return *this;
+    }
+
+    template<typename ValueType>
+    MultigraphIterator<ValueType> &MultigraphIterator<ValueType>::operator--()
+    {
+        --mapIterator;
         return *this;
     }
 }
